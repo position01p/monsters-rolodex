1cf6871346505b1fa23bfb8269741377c499a563
position01p <74172621+position01p@users.noreply.github.com>
2023-05-24 18:13:04 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
+import SearchBox from './components/search-box/search-box.component';
 
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
-    this.state={
-      monsters:[ ]
+    this.state = {
+      monsters: [],
+      searchField: ""
     }
   }
 
- componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response)=>response.json())
-  .then((users)=>
-    this.setState(
-    ()=>{return {monsters:users}},
-    ()=>console.log(this.state)
-    )
-  )
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => { return { monsters: users } }
+        )
+      )
   }
-  
-  render(){
+
+  onSearchChang = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <input
-          className='search'
-          type='search'
-          onChange={(event)=>
-            console.log(event.target.value)}
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          onChangeHandler={this.onSearchChang}
+          className={"monster-search-box"}
+          placeholder={"monster search"}
         />
-
-        {this.state.monsters.map((monster)=>{
-          return <div key={this.state.monsters.id}><h1>{monster.name} </h1></div>
+        {/* 隱藏的顯示 */}
+        {/* {filteredMonster.map((monster) => {
+          return <div key={monsters.id}><h1>{monster.name} </h1></div>
         })
-        }
-                
+        } */}
+        <CardList monsters={filteredMonster} />
       </div>
     );
   };
-  
+
 };
 
 export default App;
