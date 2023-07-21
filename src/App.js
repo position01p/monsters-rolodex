eeb35ec3f3e2add891435ce94c1d2b29a8ae725e
unknown <chu@sunrise.com>
2023-07-21 18:16:04 +0800

    go home

--- src/App.js
+++ src/App.js
@@ -1,61 +1,34 @@
 import { Component } from 'react';
-import CardList from './components/card-list/card-list.component';
+import logo from './logo.svg';
 import './App.css';
-import SearchBox from './components/search-box/search-box.component';
-
 
 class App extends Component {
-  constructor() {
+  constructor(){
     super();
-
-    this.state = {
-      monsters: [],
-      searchField: ""
+    this.state={
+      name:"Andrei"
     }
   }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => { return { monsters: users } }
-        )
-      )
-  }
-
-  onSearchChang = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField }
-    })
-  }
-
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonster = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={this.onSearchChang}
-          className={"monster-search-box"}
-          placeholder={"monster search"}
-        />
-        {/* 隱藏的顯示 */}
-        {/* {filteredMonster.map((monster) => {
-          return <div key={monsters.id}><h1>{monster.name} </h1></div>
-        })
-        } */}
-        <CardList monsters={filteredMonster} />
-      </div>
-    );
-  };
-
-};
+render(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi {this.state.name}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+}
 
 export default App;
